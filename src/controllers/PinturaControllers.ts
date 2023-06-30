99e9b956d75cab06bb48f638e13e30c9bf24cd8b
Verginia <lojavirtual@tintasverginia.com.br>
2023-06-30 08:50:33 -0300

    url-img

--- src/controllers/PinturaControllers.ts
+++ src/controllers/PinturaControllers.ts
@@ -19,7 +19,7 @@ class PinturaController {
       return {
         id: pintura.id,
         title: pintura.name,
-        photo: `https://dashboard.render.com/uploads/${pintura.photo}`,
+        photo: `https://verginia.onrender.com/uploads/${pintura.photo}`,
       }
     })
 
